d670b36b6c95a0673fcb9346373e8dd5e4cadfb3
MG <mg@mgs-macbook-air.local>
2016-01-05 09:17:20 -0800

    Initial Commit

---
+++ taxCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  taxCalculator
+//
+//  Created by MG on 1/5/16.
+//  Copyright © 2016 mobile makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
